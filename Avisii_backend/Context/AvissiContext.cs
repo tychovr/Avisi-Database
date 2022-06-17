931c127b3407afbdbc2615e0c1350f85cf136347
luuk0406 <luuk@byron.nl>
2022-06-17 12:50:44 +0200

    Add project files.

---
+++ Avisii_backend/Context/AvissiContext.cs
@@ -0,0 +1,18 @@
+﻿using Avisii_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Avisii_backend.Context
+{
+    public class AvissiContext : DbContext
+    {
+        public AvissiContext([NotNullAttribute] DbContextOptions options): base(options)
+        {
+            
+        }
+
+        DbSet<User> User { get; set; }
+        
+
+    }
+}
---
+++ Avisii_backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Avisii_backend.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+}
